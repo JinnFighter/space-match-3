10814264136435485194c6fdf7551e25bfcaf779
JinnFighter <chernayamolnia@yandex.ru>
2022-07-09 20:29:59 +0300

    chg: tileview parent

--- Assets/Scripts/View/Systems/InitTileViewSystem.cs
+++ Assets/Scripts/View/Systems/InitTileViewSystem.cs
@@ -1,6 +1,9 @@
+using Assets.Scripts.Common;
 using Assets.Scripts.Logic.Components;
 using Assets.Scripts.View.Components;
+using Assets.Scripts.View.Content;
 using Leopotam.Ecs;
+using UnityEngine;
 
 namespace Assets.Scripts.View.Systems
 {
@@ -8,13 +11,18 @@ namespace Assets.Scripts.View.Systems
     {
         private readonly EcsFilter<Tile> _filter = null;
 
+        private readonly ViewContainer _viewContainer = null;
+        private readonly PrefabsContent _prefabsContainer = null;
+
         public void Init()
         {
             foreach(var index in _filter)
             {
                 var entity = _filter.GetEntity(index);
                 ref var tileViewModel = ref entity.Get<TileViewModel>();
-                tileViewModel.State = -1;
+
+                ref var tileViewContainer = ref entity.Get<TileViewContainer>();
+                tileViewContainer.TileView = Object.Instantiate(_prefabsContainer.TileView, _viewContainer.GameplayCanvas.transform);
             }
         }
     }
