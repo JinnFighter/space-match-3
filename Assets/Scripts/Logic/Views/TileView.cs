27d390089d539cdebf32c2336fe91f4000637cb8
JinnFighter <chernayamolnia@yandex.ru>
2022-07-15 02:58:36 +0300

    TileView select and deselect methods

--- Assets/Scripts/Logic/Views/TileView.cs
+++ Assets/Scripts/Logic/Views/TileView.cs
@@ -1,9 +1,15 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TileView : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _stateText;
+    [SerializeField] private Image _stateImage;
 
     public void SetState(int state) => _stateText.text = state >= 0 ? $"{ state }" : "-";
+
+    public void Select() => _stateImage.color = new Color(_stateImage.color.r, _stateImage.color.g, _stateImage.color.b, 255);
+
+    public void Deselect() => _stateImage.color = new Color(_stateImage.color.r, _stateImage.color.g, _stateImage.color.b, 128);
 }
