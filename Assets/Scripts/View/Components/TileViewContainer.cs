d03758ab6998d475fcb09de8f27c8213b783af25
JinnFighter <chernayamolnia@yandex.ru>
2022-07-15 01:56:27 +0300

    rem: unused View asmdef

--- Assets/Scripts/View/Components/TileViewContainer.cs
+++
@@ -1,9 +0,0 @@
-using Assets.Scripts.View.Views;
-
-namespace Assets.Scripts.View.Components
-{
-    public struct TileViewContainer
-    {
-        public TileView TileView;
-    }
-}
--- Assets/Scripts/View/Views/GameFieldView.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class GameFieldView : MonoBehaviour
-{
-
-}
