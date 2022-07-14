4c5fc52d751f08715cca460dcd2861bb9e9a4d81
JinnFighter <chernayamolnia@yandex.ru>
2022-07-15 02:10:22 +0300

    add: missing view scripts

--- Assets/Scripts/Logic/Content/PrefabsContent.cs
+++ Assets/Scripts/Logic/Content/PrefabsContent.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PrefabsContent", menuName = "Content/PrefabsContent")]
-public class PrefabsContent : ScriptableObject
+namespace Assets.Scripts.Logic.Content
 {
+    [CreateAssetMenu(fileName = "PrefabsContent", menuName = "Content/PrefabsContent")]
+    public class PrefabsContent : ScriptableObject
+    {
+    }
 }
--- Assets/Scripts/Logic/Views/GameFieldView.cs
+++ Assets/Scripts/Logic/Views/GameFieldView.cs
@@ -1,5 +1,12 @@
+using Assets.Scripts.Common;
 using UnityEngine;
 
-public class GameFieldView : MonoBehaviour
+namespace Assets.Scripts.Logic.Views
 {
+    public class GameFieldView : MonoBehaviour
+    {
+        [SerializeField] private FlexibleGridLayout _layout;
+
+        public void SetWidth(int width) => _layout.constrainChildrenSizeToSizeAtCount = width;
+    }
 }
