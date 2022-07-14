a9ddb4bdeeecfd6e9f5aeee9d7ecc2a98f7f2249
JinnFighter <chernayamolnia@yandex.ru>
2022-07-15 01:49:40 +0300

    chg: moved IGame to Logic asmdef

--- Assets/Scripts/Init/IInit.cs
+++ Assets/Scripts/Init/IInit.cs
@@ -1,9 +1,10 @@
 using Assets.Scripts.Common;
 
-namespace Assets.Scripts.Init
+namespace Assets.Scripts.Logic
 {
     public interface IInit
     {
         void Init(IGame game);
     }
 }
+
--- Assets/Scripts/Init/InitContainer.cs
+++ Assets/Scripts/Init/InitContainer.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Logic;
 using System.Collections;
 using System.Collections.Generic;
 
