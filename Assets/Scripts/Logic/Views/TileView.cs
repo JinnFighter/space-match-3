8892abf0216184a5f10ce0a64f9f17f9f31dd67b
JinnFighter <chernayamolnia@yandex.ru>
2022-07-15 13:11:59 +0300

    add: TileView.IsClicked

--- Assets/Scripts/Logic/Views/TileView.cs
+++ Assets/Scripts/Logic/Views/TileView.cs
@@ -1,18 +1,28 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.Logic.Views
 {
-    public class TileView : MonoBehaviour
+    public class TileView : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField] private TextMeshProUGUI _stateText;
         [SerializeField] private Image _stateImage;
+
+        public bool IsClicked { get; private set; }
 
         public void SetState(int state) => _stateText.text = state >= 0 ? $"{state}" : "-";
 
         public void Select() => _stateImage.color = new Color(_stateImage.color.r, _stateImage.color.g, _stateImage.color.b, 255);
 
         public void Deselect() => _stateImage.color = new Color(_stateImage.color.r, _stateImage.color.g, _stateImage.color.b, 128);
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            IsClicked = true;
+        }
+
+        public void ResetClicked() => IsClicked = false;
     }
 }
