ef7ca45a1f1f690abb77d0569bb73c08aa95053a
JinnFighter <chernayamolnia@yandex.ru>
2022-07-15 16:23:14 +0300

    fix: tile swap on diagonals

--- Assets/Scripts/Logic/Models/GameFieldModel.cs
+++ Assets/Scripts/Logic/Models/GameFieldModel.cs
@@ -11,6 +11,16 @@ namespace Assets.Scripts.Logic.Models
 
         public bool IsInside(Vector2Int position) => position.x >= 0 && position.x < Width && position.y >= 0 && position.y < Height;
 
-        public bool IsAdjacent(Vector2Int firstPos, Vector2Int secondPos) => firstPos != secondPos && IsInside(firstPos) && IsInside(secondPos) && Mathf.Abs(firstPos.x - secondPos.x) <= 1 && Mathf.Abs(firstPos.y - secondPos.y) <= 1;
+        public bool IsAdjacent(Vector2Int firstPos, Vector2Int secondPos)
+        {
+            bool IsAdjacentExceptDiagonal (Vector2Int firstPos, Vector2Int secondPos)
+            {
+                var dx = Mathf.Abs(firstPos.x - secondPos.x);
+                var dy = Mathf.Abs(firstPos.y - secondPos.y);
+                return dx <= 1 && dy <= 1 && (dx + dy) % 2 == 1;
+            }
+
+            return firstPos != secondPos && IsInside(firstPos) && IsInside(secondPos) && IsAdjacentExceptDiagonal(firstPos, secondPos);
+        }
     }
 }
