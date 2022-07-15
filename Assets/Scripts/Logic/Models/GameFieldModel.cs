149ff90fea1358becce0281781fa5e1b5bdc18ab
JinnFighter <chernayamolnia@yandex.ru>
2022-07-15 13:37:14 +0300

    add: GameFieldModel.IsAdjacent

--- Assets/Scripts/Logic/Models/GameFieldModel.cs
+++ Assets/Scripts/Logic/Models/GameFieldModel.cs
@@ -10,5 +10,7 @@ namespace Assets.Scripts.Logic.Models
         public int Height => Tiles.GetLength(1);
 
         public bool IsInside(Vector2Int position) => position.x >= 0 && position.x < Width && position.y >= 0 && position.y < Height;
+
+        public bool IsAdjacent(Vector2Int firstPos, Vector2Int secondPos) => firstPos != secondPos && IsInside(firstPos) && IsInside(secondPos) && Mathf.Abs(firstPos.x - secondPos.x) <= 1 && Mathf.Abs(firstPos.y - secondPos.y) <= 1;
     }
 }
