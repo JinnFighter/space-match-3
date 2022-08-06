b337bbbd2ee2c5be9a5271a2336ba71d626be6ad
JinnFighter <chernayamolnia@yandex.ru>
2022-08-06 20:40:02 +0300

    chg: moved tile data from TileStatesDescription to GameFieldDescription

--- Assets/Scripts/Logic/Descriptions/DescriptionsContent.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DescriptionsContent", menuName = "Descriptions/DescriptionsContent")]
-public class DescriptionsContent : ScriptableObject
-{
-    [field: SerializeField] public GameFieldDescription GameFieldDescription { get; private set; }
-    [field: SerializeField] public TileStatesDescription TileStatesDescription { get;private set; }
-}
