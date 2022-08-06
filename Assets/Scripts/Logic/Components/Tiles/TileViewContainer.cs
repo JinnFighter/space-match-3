a56fcf9f0fb7a46a0a31990d0333334e9ddd223d
JinnFighter <chernayamolnia@yandex.ru>
2022-08-06 21:35:05 +0300

    add: Tile component

--- Assets/Scripts/Logic/Components/Tiles/TileViewContainer.cs
+++ Assets/Scripts/Logic/Components/Tiles/TileViewContainer.cs
@@ -1,8 +1,7 @@
 using Assets.Scripts.Logic.Views;
-using System;
+
 namespace Assets.Scripts.Logic.Components.Tiles
 {
-    [Serializable]
     public struct TileViewContainer
     {
         public TileView TileView;
--- Assets/Scripts/Logic/Systems/GameField/InitGameFieldViewSystem.cs
+++ Assets/Scripts/Logic/Systems/GameField/InitGameFieldViewSystem.cs
@@ -8,10 +8,11 @@ namespace Assets.Scripts.Logic.Systems.GameField
 {
     public class InitGameFieldViewSystem : IEcsInitSystem
     {
-        private readonly GameFieldModel _gameFieldModel;
-        private readonly ViewContainer _viewContainer;
-        private readonly GameFieldView _gameFieldPrefab;
-        private readonly TileView _tilePrefab;
+        private readonly EcsWorld _world = null;
+        private readonly GameFieldModel _gameFieldModel = null;
+        private readonly ViewContainer _viewContainer = null;
+        private readonly GameFieldView _gameFieldPrefab = null;
+        private readonly TileView _tilePrefab = null;
 
         public void Init()
         {
@@ -23,6 +24,13 @@ namespace Assets.Scripts.Logic.Systems.GameField
                 for (int j = 0; j < _gameFieldModel.Height; j++)
                 {
                     var tileView = Object.Instantiate(_tilePrefab, parentTransform);
+                    var tileEntity = _world.NewEntity();
+                    ref var tile = ref tileEntity.Get<Tile>();
+                    tile.Position = new Vector2Int(i, j);
+                    tile.State = _gameFieldModel.Tiles[i, j];
+                    tileView.SetState(tile.State);
+                    ref var tileViewContainer = ref tileEntity.Get<TileViewContainer>();
+                    tileViewContainer.TileView = tileView;
                 }
             }
         }
