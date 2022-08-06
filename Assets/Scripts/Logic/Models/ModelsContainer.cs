8cb34b0e01584b661fe09dd41f1abc5fea187ff6
JinnFighter <chernayamolnia@yandex.ru>
2022-08-06 20:29:34 +0300

    rem: MVC code

--- Assets/Scripts/Logic/Models/ModelsContainer.cs
+++
@@ -1,7 +0,0 @@
-using Assets.Scripts.Logic.Models;
-
-public class ModelsContainer
-{
-    public GameFieldModel GameFieldModel { get; private set; } = new GameFieldModel();
-    public SelectedTilesModel SelectedTilesModel { get; private set; } = new SelectedTilesModel();
-}
