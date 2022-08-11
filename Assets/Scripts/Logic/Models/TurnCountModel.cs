b0238b66311d4b012dc771cef15152a3b73ab4c5
JinnFighter <chernayamolnia@yandex.ru>
2022-08-12 01:12:09 +0300

    add: TurnCountModel events

--- Assets/Scripts/Logic/Models/TurnCountModel.cs
+++ Assets/Scripts/Logic/Models/TurnCountModel.cs
@@ -1,7 +1,24 @@
+using System;
+
 namespace Assets.Scripts.Logic.Models
 {
     public class TurnCountModel
     {
-        public int TurnCount { get; set; } = 20;
+        private int _turnCount = 20;
+
+        public int TurnCount
+        {
+            get => _turnCount;
+            set
+            {
+                if(_turnCount != value)
+                {
+                    _turnCount = value;
+                    TurnCountChanged?.Invoke(_turnCount);
+                }
+            }
+        }
+
+        public event Action<int> TurnCountChanged;
     }
 }
