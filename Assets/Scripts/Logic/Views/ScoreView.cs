01cf84e7a8c3a05009a6ae2d292125b6c8435529
JinnFighter <chernayamolnia@yandex.ru>
2022-08-11 16:04:06 +0300

    add: ScoreView

---
+++ Assets/Scripts/Logic/Views/ScoreView.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.Logic.Views
+{
+    public class ScoreView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _scoreText;
+
+        public void SetScore(int score) => _scoreText.text = $"{ score }";
+    }
+}
--- Assets/Scripts/Logic/Views/UiView.cs
+++ Assets/Scripts/Logic/Views/UiView.cs
@@ -4,5 +4,6 @@ namespace Assets.Scripts.Logic.Views
 {
     public class UiView : MonoBehaviour
     {
+        [field: SerializeField] public ScoreView ScoreView { get; private set; }
     }
 }
