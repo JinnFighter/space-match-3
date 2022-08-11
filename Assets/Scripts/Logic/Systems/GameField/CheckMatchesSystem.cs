2ecd11fabcaf53aa873882f6affcb2d429653424
JinnFighter <chernayamolnia@yandex.ru>
2022-08-11 15:29:35 +0300

    chg: CheckMatchesSystem logic

--- Assets/Scripts/Logic/Systems/GameField/CheckMatchesSystem.cs
+++ Assets/Scripts/Logic/Systems/GameField/CheckMatchesSystem.cs
@@ -9,26 +9,29 @@ namespace Assets.Scripts.Logic.Systems.GameField
     public class CheckMatchesSystem : IEcsRunSystem
     {
         private readonly EcsWorld _world = null;
-        private readonly EcsFilter<TurnEvent> _filter = null;
+        private readonly EcsFilter<CheckMatchRequest> _filter = null;
         private readonly GameFieldModel _gameFieldModel = null;
+        private readonly Vector2Int[] _moveDirections = new Vector2Int[4] { Vector2Int.left, Vector2Int.right, Vector2Int.up, Vector2Int.down };
 
         public void Run()
         {
-            if(!_filter.IsEmpty())
+            foreach(var index in _filter)
             {
-                var directions = new Vector2Int[4] { Vector2Int.left, Vector2Int.right, Vector2Int.up, Vector2Int.down };
                 var visitStates = new bool[_gameFieldModel.Width, _gameFieldModel.Height];
                 for (var i = 0; i < _gameFieldModel.Width; i++)
                 {
                     for (int j = 0; j < _gameFieldModel.Height; j++)
                     {
-                        var matches = GetMatches(_gameFieldModel[i, j].Position, directions, visitStates);
+                        var matches = GetMatches(_gameFieldModel[i, j].Position, _moveDirections, visitStates);
                         if (matches.Count >= 3)
                         {
                             _world.SendMessage(new MatchEvent { MatchPositions = matches });
                         }
                     }
                 }
+
+                var entity = _filter.GetEntity(index);
+                entity.Del<CheckMatchRequest>();
             }
         }
 
