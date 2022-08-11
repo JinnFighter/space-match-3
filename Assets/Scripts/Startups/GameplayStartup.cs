62cd1a41e695d7d873dbdebad0f3e14eb7131f2a
JinnFighter <chernayamolnia@yandex.ru>
2022-08-12 02:38:01 +0300

    rem: turn count ui systems

--- Assets/Scripts/Startups/GameplayStartup.cs
+++ Assets/Scripts/Startups/GameplayStartup.cs
@@ -98,7 +98,6 @@ namespace SpaceMatch3
                 .Add(new ShiftTilesSystem())
                 .Add(new UpdateTileStatesSystem())
                 .Add(new UpdateTileSelectionSystem())
-                .Add(new UpdateTurnCountViewSystem())
                 .Add(new EnableGameOverViewSystem());
         }
 
