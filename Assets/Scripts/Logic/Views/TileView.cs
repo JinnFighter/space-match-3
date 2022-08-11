05b84a01d74fc8bfce5d5ed3d7fa6b60fa521c03
JinnFighter <chernayamolnia@yandex.ru>
2022-08-11 21:23:27 +0300

    add: TileView.Ball-related methods

--- Assets/Scripts/Logic/Views/TileView.cs
+++ Assets/Scripts/Logic/Views/TileView.cs
@@ -9,10 +9,29 @@ namespace Assets.Scripts.Logic.Views
     {
         [SerializeField] private TextMeshProUGUI _stateText;
         [SerializeField] private Image _stateImage;
+        [SerializeField] private Image _ballImage;
+
+        private GameObject _ballGameObject;
+        public GameObject BallGameObject 
+        { 
+            get
+            {
+                if(_ballGameObject == null)
+                {
+                    _ballGameObject = _ballImage.gameObject;
+                }
+
+                return _ballGameObject;
+            } 
+        }
 
         public bool IsClicked;
 
         public void SetState(int state) => _stateText.text = state >= 0 ? $"{state}" : "-";
+        public void SetColor(Color color) => _ballImage.color = color;
+
+        public void EnableBall() => BallGameObject.SetActive(true);
+        public void DisableBall() => BallGameObject.SetActive(false);
 
         public void Select() => _stateImage.color = new Color(_stateImage.color.r, _stateImage.color.g, _stateImage.color.b, 1f);
 
